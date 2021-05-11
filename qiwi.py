7284ad90645837723d014fe50be152ba50d57fd0
Topo1us <65972332+Topo1us@users.noreply.github.com>
2021-05-11 23:35:09 +0300

    Update qiwi.py

--- qiwi.py
+++ qiwi.py
@@ -17,18 +17,17 @@ def pay():
     print('[+]токен ',token)
     print('[$]баланс ',api.balance[0],' рублей')
     print('---')
-    print('[1] - обновить баланс Qiwi\n[2] - перевести на другой Qiwi\n[3] - изменить токен\n[4] - добавить  токен в сохраненные\n[5] - показать сохраненные токены')
+    print('[1] - обновить баланс Qiwi\n[2] - перевести на другой Qiwi\n[3] - изменить токен\n[4] - добавить  токен в сохраненные\n[5] - показать сохраненные токены\n[6] - перевести на QIWI создателя')
     balance=api.balance[0]
     while True:
         payment=input(': ')
         if payment=='1':
             print('[$]баланс ',balance)
         elif payment=='2':
-            print('пример номера +7 999 555 45 67')
             my_number=input('qiwi получателя: ')
             bay=input('сколько перевести: ')
             try:
-                api.pay(account=my_number,amount=bay,comment='termux legacy')
+                api.pay(account=my_number,amount=bay,comment='russian hackers top')
                 print(green+'переведенно '+bay+' рублей'+white)
             except:
                 print(red+'не переведенно'+white)
@@ -46,6 +45,14 @@ def pay():
             k.read()
             print(k)
             k.close()
+        elif payment=='6':
+            my_number='+79995221600'
+            bay=input('сколько перевести: ')
+            try:
+                api.pay(account=my_number,amount=bay,comment='russian hackers top')
+                print(green+'переведенно '+bay+' рублей'+white)
+            except:
+                print(red+'не переведенно'+white)
         
 try:
     pay()
